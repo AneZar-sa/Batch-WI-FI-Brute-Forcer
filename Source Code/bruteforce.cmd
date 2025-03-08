e1a751f826a6a1aab88926f044190a9ee4d96ac3
AneZar-sa <anesimamovic2023@outlook.com>
2025-03-08 06:22:10 +0100

    Update bruteforce.cmd

--- Source Code/bruteforce.cmd
+++ Source Code/bruteforce.cmd
@@ -395,7 +395,8 @@
 			if %%a==SSID (
 				set /a keynumber=!keynumber! + 1
 				set current_ssid=%%d
-
+                        if !current_ssid!=="" (
+                                set current_ssid
 				call :character_finder_2 "!current_ssid!"
 
 			)
